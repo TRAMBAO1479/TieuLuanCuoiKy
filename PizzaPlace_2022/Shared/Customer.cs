8803658200efc90d77ec9b9204c3d579786ff8d0
TRAMBAO1479 <nguyenthibaotram44@gmail.com>
2022-06-26 23:20:55 +0700

    Add project files.

---
+++ PizzaPlace_2022/Shared/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PizzaPlace_2022.Shared {
+    public class Customer {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Nhập tên của bạn ở đây nè!")]
+        public string Name { get; set; } = default!;
+        [Required(ErrorMessage = "Vui lòng nhập địa chỉ!")]
+        public string Street { get; set; } = default!;
+        [Required(ErrorMessage = "Làm ơn nhập tên thành phố bạn đang sống")]
+        public string City { get; set; } = default!;
+
+        [Required(ErrorMessage = "Vui lòng nhập SĐT")]
+        public string SDT { get; set; } = default!;
+
+        [Required(ErrorMessage = "Vui lòng nhập ngày sinh của bạn!")]
+        public string DOB { get; set; } = default!;
+    }
+}
---
+++ PizzaPlace_2022/Shared/Spiciness.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaPlace_2022.Shared {
+    public enum Spiciness {
+        None,
+        Spicy,
+        Hot,
+        Cuba,
+        Coffee,
+        Bloody,
+        cake,
+        Mojito,
+        maitai
+    }
+}
